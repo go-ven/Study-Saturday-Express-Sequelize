4a0ce1b2fb8ff5ec8a7071c7d91b41fe8d345277
SagiGo <sagigo@gmail.com>
2020-05-02 12:35:57 -0400

    unresolved spec issues

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/study-saturdays',
+  process.env.DATABASE_URL || 'postgres://localhost:5432/study-saturdays1',
   {
     logging: false,
   }
--- db/models/test.js
+++ db/models/test.js
@@ -3,6 +3,23 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false       //validate
+  },
+  grade : {
+    type : Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+//associations. ways to tell dbhow model are related to each other. test belongs to exactly 1 student. so gice each test a studet id
+//hasMany
+//belongsToMany
+
+Test.belongsTo(Student, { as: 'student'}) //will add student id to test
+//as: student will passed AS "student".
+
 
 module.exports = Test;
